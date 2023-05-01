a4e9b25f318414e9ed1b096fda59aa97cdab419a
Centli <cgarcesbuendia@appen.com>
2023-04-30 19:11:39 -0600

    MODIFY project files in order to have a clean and legible code

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,51 +1,4 @@
-# import boto3
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# client=boto3.resource('s3')
-# bucket_name = 'boto3-files-bucket-radio-task'
-
-# @app.get("/")
-# def home():
-#     return {"Radio": "Station"}
-
-# @app.get("/audio")
-# def audio_files():
-#     bucket=client.Bucket(bucket_name)
-#     files = bucket.objects.all()
-#     file_list = []
-
-#     for file in files:
-#         if file.key.endswith('.wav') or file.key.endswith('.mp3') or file.key.endswith('.flac'): 
-#             file_list.append(file.key)
-#     return(file_list)
-
-# @app.get("/video")
-# def audio_files():
-#     bucket=client.Bucket(bucket_name)
-#     files = bucket.objects.all()
-#     file_list = []
-
-#     for file in files:
-#         if file.key.endswith('.mp4') or file.key.endswith('.avi') or file.key.endswith('.mov'): 
-#             file_list.append(file.key)
-#     return(file_list)
-
-# @app.get("/documents")
-# def audio_files():
-#     bucket=client.Bucket(bucket_name)
-#     files = bucket.objects.all()
-#     file_list = []
-
-#     for file in files:
-#         if file.key.endswith('.png') or file.key.endswith('.jpg') or file.key.endswith('.gif') or file.key.endswith('.jpeg'): 
-#             file_list.append(file.key)
-#     return(file_list)
-
 import boto3
-
-# from typing import Union
 
 from fastapi import FastAPI
 
@@ -53,17 +6,6 @@ app = FastAPI()
 
 client=boto3.resource('s3')
 bucket_name = 'boto3-files-bucket-radio-task'
-
-
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
 
 
 @app.get("/")
